f480c562dbd5d76c3931d252a02835f7136ffba1
demoscloud <60758285+demoscloud@users.noreply.github.com>
2020-02-23 17:12:06 -0500

    Add files via upload

--- client2.c
+++ client2.c
@@ -3,10 +3,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
 int selectCommand();
-void connectServer();
+int connectServer();
 void list();
 void retrieve();
 void store();
@@ -17,80 +17,110 @@ void error(char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int i = 0;
-    i = selectCommand();
-    if(i == 1){
-        connectServer();
+    int sockfd;
+    while (i >= 0)
+    {
+        i = selectCommand();
+        if (i == 1)
+        {
+            sockfd = connectServer();
+        }
+        else if (i == 2)
+        {
+            list();
+        }
+        else if (i == 3)
+        {
+            retrieve();
+        }
+        else if (i == 4)
+        {
+            store();
+        }
+        else if (i == 5)
+        {
+            quit();
+        }
     }
-    else if(i == 2){
-        list();
-    }
-    else if(i == 3){
-        retrieve();
-    }
-    else if(i == 4){
-        store();
-    }
-    else if(i == 5){
-        quit();
-    }
-    else{
-        printf("\nerror executing command\n");
-        exit(0);
-    }
+    printf("\nerror executing command\n");
     return 0;
 }
 
-int selectCommand(){
+int selectCommand()
+{
     int n = -1;
     int i = 0;
     char str[5];
-    while(n == -1 && i < 5){
+    while (n == -1 && i < 5)
+    {
         printf("1.Connect\n2.List\n3.Retrieve\n4.Store\n5.Quit\n");
         printf("Input Command:");
         scanf("%s", &str);
         n = atoi(str);
-        if(n == 0 || n > 5){
+        if (n == 0 || n > 5)
+        {
             printf("\nInvalid Input, try again\n");
             n = -1;
         }
         i++;
     }
-    
+
     return n;
 }
 
-void connectServer(){
+int connectServer()
+{
     char host[20];
     char port[10];
     int portNum = -1;
-    int sockfd;
+    int sockfd = 0;
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    //user input for port and host
     printf("\nInput Hostname:");
     scanf("%s", &host);
     printf("Input Port Number:");
     scanf("%s", &port);
     portNum = atoi(port);
-    if(portNum <= 0){
-        printf("\nInvalid Port Number\n");
+    if (portNum <= 0)
+    {
+        error("\nInvalid Port Number\n");
     }
-    /*
-    else{
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if(sockfd < 0)
-            error("Error opening socket")
+    //attempt connection
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("Error opening socket");
+    server = gethostbyname(host);
+    if(server == NULL){
+        error("ERROR no such host\n");
     }
-    */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portNum);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    return sockfd;
 }
-void list(){
+void list()
+{
     printf("\nlist\n");
 }
-void retrieve(){
+void retrieve()
+{
     printf("\nretrieve\n");
 }
-void store(){
+void store()
+{
     printf("\nstore\n");
 }
-void quit(){
-    printf("\nquit\n");
+void quit()
+{
+    printf("\nExiting Now\n");
+    exit(0);
 }
