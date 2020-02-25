52eef53e89158e4700358930b3bd286d7456ceca
demoscloud <60758285+demoscloud@users.noreply.github.com>
2020-02-25 17:39:21 -0500

    Add files via upload

---
+++ Client/client.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int selectCommand();
+int connectServer();
+void list(int sockfd);
+void retrieve(int sockfd);
+void store(int sockfd);
+void quit();
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    int sockfd = 0;
+    while (i >= 0)
+    {
+        i = selectCommand();
+	//only sets socket if user input is 1 and sockfd hasn't been connected prior
+        if (i == 1 && 0 == sockfd)
+        {
+            sockfd = connectServer();
+        }
+	//list function
+        else if (i == 2)
+        {
+		if (0 != sockfd) //only attempts list function is socket has been connected
+            list(sockfd);
+		else i = 0;  //if not connected, goes back to the start of the loop and picks a new command
+        }
+        else if (i == 3)
+        {
+            retrieve(sockfd);
+        }
+        else if (i == 4)
+        {
+            store(sockfd);
+        }
+        else if (i == 5)
+        {
+            quit();
+        }
+    }
+    printf("\nerror executing command\n");
+    return 0;
+}
+
+int selectCommand()
+{
+    int n = -1;
+    int i = 0;
+    char str[5];
+    while (n == -1 && i < 5)
+    {
+        printf("1.Connect\n2.List\n3.Retrieve\n4.Store\n5.Quit\n");
+        printf("Input Command:");
+        scanf("%s", &str);
+        n = atoi(str);
+        if (n == 0 || n > 5)
+        {
+            printf("\nInvalid Input, try again\n");
+            n = -1;
+        }
+        i++;
+    }
+
+    return n;
+}
+
+int connectServer()
+{
+    char host[20];
+    char port[10];
+    int portNum = -1;
+    int sockfd = 0;
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    //user input for port and host
+    printf("\nInput Hostname:");
+    scanf("%s", &host);
+    printf("Input Port Number:");
+    scanf("%s", &port);
+    portNum = atoi(port);
+    if (portNum <= 0)
+    {
+        error("\nInvalid Port Number\n");
+    }
+    //attempt connection
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("Error opening socket");
+    server = gethostbyname(host);
+    if(server == NULL){
+        error("ERROR no such host\n");
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portNum);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    return sockfd;
+}
+void list(int sockfd)
+
+{
+	//this is the file we write two
+	FILE* fp;
+	char buffer[255];
+	//opens the file
+	fp = fopen("gotThis.txt", "w");
+	//send a command to the server "2"
+	write(sockfd,"2", 18);
+	//reads from socket and outputs to screen
+	while ( read(sockfd, buffer, 255) > 0){
+       //prints file names from socket
+	printf("\n%s", buffer);
+	
+	fputs(buffer, fp);
+	}
+	//closes file
+	fclose(fp);
+	//removes unnecessary file that has list of server's files
+	system("rm gotThis.txt");
+    
+}
+/**
+ * funciton to get a file from the server
+ * */
+void retrieve(int sockfd)
+{
+    printf("\nretrieve\n");
+
+    FILE *fp;
+    char buffer[256];
+    char *fileName;	//name of file to be read in.  256 char limit
+    fileName = (char*) malloc(sizeof(255));
+    int c;
+
+    do {
+	    //gets filename from upser
+	printf("Enter file name to retrieve: \n");
+	scanf("%s", fileName);
+	//sends file request to server
+	write(sockfd,  fileName, 256);
+	//creates file
+	fp= fopen(fileName, "w");
+	//scans input from socket until no more info is sent
+	while (read (sockfd, buffer, 256) > 0){
+		fputs(buffer, fp);
+	}
+
+	c = fgetc(fp);//checks if last character in file is empty
+//	if (c ==EOF){
+//	free(fp); //deletes empty file
+	}
+    //if last character in file indicates the end of the file, it stops reading the file
+    while (c !=EOF || '3'== buffer[0]);
+	fclose(fp);
+	//memory management for file name and buffer
+    free(fileName);
+    }	    
+
+    /**
+     * store function moves file from client to server
+     * */
+void store(int sockfd)
+{
+	FILE *fp;
+	char buffer[256] = {0};
+	char *fileName;
+	fileName = (char*) malloc(sizeof(255));
+    printf("\nEnter name of file to store: \n");
+    scanf("%s", fileName);
+    
+	//if user passes a valid file name, send the file
+	if (NULL != fopen(fileName, "r")){
+		fp = fopen(fileName, "r");
+		//sends a message to the server to go to the 'store' option
+		write(sockfd, "4", 20);	//step 1
+		buffer[0]='0';
+		//keeps reading from the socket until the server is ready to recieve a file.
+		while ('0' == buffer[0]){//step 4
+			read(sockfd, buffer, 256);
+		}
+		//writes sends the file name to the server
+		while ('4' == buffer[0]){
+		write(sockfd, fileName, 255);//step 5
+		read(sockfd, buffer, 255);//step 7
+		}
+		//puts contents of file into socket
+		while (fscanf(fp, "%s ", buffer) != EOF){//step 8
+			write(sockfd, buffer, 256);
+			write(sockfd, " ", 20);
+		}
+		fclose(fp);
+		}
+}
+void quit()
+{
+    printf("\nExiting Now\n");
+    exit(0);
+}
--- server2.c
+++ server2.c
@@ -1,11 +1,5 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
-   process for each connection
-   gcc server2.c -lsocket
-*/
 #include <stdio.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -20,42 +14,48 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen, pid;
-     struct sockaddr_in serv_addr, cli_addr;
+    int sockfd, newsockfd, portno, clilen, pid;
+    struct sockaddr_in serv_addr, cli_addr;
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     while (1) {
-         newsockfd = accept(sockfd, 
-               (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
-             error("ERROR on accept");
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
-     } /* end of while */
-     return 0; /* we never get here */
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    while (1)
+    {
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        pid = fork();
+        if (pid < 0)
+            error("ERROR on fork");
+        if (pid == 0)
+        {
+            close(sockfd);
+            dostuff(newsockfd);
+            exit(0);
+        }
+        else
+        {
+            close(newsockfd);
+        }
+    }         /* end of while */
+    return 0; /* we never get here */
 }
 
 /******** DOSTUFF() *********************
@@ -63,56 +63,129 @@ int main(int argc, char *argv[])
  for each connection.  It handles all communication
  once a connnection has been established.
  *****************************************/
-void dostuff (int sock)
+void dostuff(int sock)
 {
-//	printf("entered doStuff\n");//debug line
-   int n;
-   char buffer[256];
-   char b[255];
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   //start Olivia Code
-   FILE* fp; //file pointer for file created 
-   //checks if user inputs command 2 for list
-   if ('2'== buffer[0]){
-	   //puts all the output into a new file 
-	   system("ls >> dummyFile.txt");
-	   fp = fopen("dummyFile.txt", "r");
-	  
-	while(fscanf(fp, "%s ", b)!= EOF ){
-		n = write(sock, b, 255);
-		n = write(sock, " ", 255);	
-		if (n<0) error("fscanf failed to write to socket");
-	}	
+    //	printf("entered doStuff\n");//debug line
+    int n;
+    char buffer[256] = {0};
+    char b[256];
+    bzero(buffer, 256);
+    bzero(b, 256);
+    n = read(sock, buffer, 255);
+    if (n < 0)
+        error("ERROR reading from socket");
+    //start Olivia Code
+    FILE *fp; //file pointer for file created
+    //checks if user inputs command 2 for list
+    if ('2' == buffer[0])
+    {
+        //puts all the output into a new file
+        system("ls >> dummyFile.txt");
+        fp = fopen("dummyFile.txt", "r");
 
-	fclose(fp);
-	system("rm dummyFile.txt");
-   }
-   //takes in a file name and if it's valid, returns the file to client
-   else if (NULL != fopen(buffer, "r")){
-	fp = fopen(buffer, "r");
-	//checks that input is valid file
-	if (fp != NULL){
-		while(fscanf(fp, "%s ", b) != EOF){
-			n = write(sock, b, 255);
-			n = write(sock, " ", 255);
-		}				
-	}
-      	fclose(fp);
-   }
-   //store function takes files from client and stores on server side
-   else{
-	fp = fopen("clientFile.txt", "w");
-	int c;
-	do{
-		read(sock, buffer, 255);
-		fputs(buffer, fp);
-		fputs(" ", fp);
-		c = fgetc(buffer);
-	}
-	while (c != EOF);
-	fclose(fp);
-   }
-   //end Olivia code
+        while (fscanf(fp, "%s ", b) != EOF)
+        {
+            n = write(sock, b, 255);
+            n = write(sock, " ", 255);
+            if (n < 0)
+                error("fscanf failed to write to socket");
+        }
+
+        fclose(fp);
+        system("rm dummyFile.txt");
+        n = write(sock, "exit", 4);
+    }
+
+    //retrieve function
+    else if ('4' == buffer[0])
+    {
+        int c = 0;
+        char *fileName;
+        fileName = (char *)malloc(sizeof(256));
+        printf("Got a request to store a file\n");
+        system("sleep 5s");
+        //asks client for name of file
+        write(sock, "4", 20); //step 3
+        //reads file name from socket
+        while ('4' == buffer[0])
+        {
+            read(sock, buffer, 256);
+            //opens a file with requested name
+            fp = fopen(buffer, "w");
+            //once fileName is caught, sends signal to client to stop
+            write(sock, "1", 20); //step 6
+        }
+        //Amber edit
+        bzero(buffer, 255);
+        while(strcmp(buffer, "exit") != 0){
+
+            bzero(buffer, 255);
+            read(sock, buffer, 255);
+            printf("recieved: %s\n", buffer);
+            write(sock, "recieved", 20);
+            printf("ack sent\n");
+            if(strcmp(buffer, "exit") == 0){
+                break;
+            }
+            fprintf(fp, "%s", buffer);
+        }
+/*
+        //reads from stocket to newly created file
+        while (read(sock, buffer, 256) > 0 && c != EOF)
+        {
+            printf("%s", buffer);
+            fputs(buffer, fp);
+            c = fgetc(fp);
+        }
+*/
+        printf("Finished reading file\n\n");
+
+        //closes file
+        fclose(fp);
+    }
+
+    //takes in a file name and if it's valid, returns the file to client
+    else if (NULL != fopen(buffer, "r"))
+    {
+        fp = fopen(buffer, "r");
+        //checks that input is valid file
+        if (fp != NULL)
+        {
+            while (fscanf(fp, "%s ", b) != EOF)
+            {
+                n = write(sock, b, 255);
+                n = write(sock, " ", 255);
+            }
+        }
+        fclose(fp);
+    }
+    //store function takes files from client and stores on server side
+    //if the user put in an invalid file to retrieve, the server will create a trash file
+    //with the contents being that filename
+    else
+    {
+
+        fp = fopen("clientFile.txt", "w");
+        int c;
+        do
+        {
+            read(sock, buffer, 255);
+            fputs(buffer, fp);
+            fputs(" ", fp);
+            c = fgetc(buffer);
+        } while (c != EOF);
+        fclose(fp);
+    }
+    //end Olivia code
+
+    //check for more commands
+    bzero(buffer, 255);
+    n = read(sock, buffer, 255);
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("buffer: %s\n", buffer);
+    if ('1' == buffer[0])
+    {
+        dostuff(sock);
+    }
 }
